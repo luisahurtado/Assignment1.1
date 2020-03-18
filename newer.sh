4aad5b65b14aced4344d6bac08c71f241e15d7ba
luisahurtado <61818251+luisahurtado@users.noreply.github.com>
2020-03-18 14:42:28 +1100

    Add files via upload
    The answer question 1 of the assignment 1 task 1

---
+++ newer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Question 1 of the assignment 1.
+#The script resturns the name of the newest file
+#from the documents given as the input
+#Save as newer.sh
+documents=$(ls -t $* |head -n 1)
+echo "The newer file in the list is $documents"
