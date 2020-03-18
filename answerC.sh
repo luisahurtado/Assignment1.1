3066804bb37bad5a61e5241e93d7feb75234a684
luisahurtado <61818251+luisahurtado@users.noreply.github.com>
2020-03-18 14:44:25 +1100

    Add files via upload
    The answer to question 3.c of assignment 1 task 1

---
+++ answerC.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Modification of the original code
+# First, gives a time signal every hour when connected to cron
+# then, there is a pause of 3 seconds
+time=$(date +%I)
+minutes=$(date +%M)
+count=0
+while [ $count -lt $time ]; do
+  echo -e "\a"
+  sleep 1 # sleep for one second
+  count=$[$count+1]
+done
+sleep 3
+if [ $minutes -lt 30 ] && [ $minutes -ge 15 ]; then
+	echo -e "\a"
+   elif [ $minutes -lt 45 ] && [ $minutes -ge 30 ]; then
+	echo -e "\a"
+	sleep 1
+	echo -e "\a"
+   elif [ $minutes -ge 59 ] && [ $minutes -le 45 ]; then
+	echo -e "\a"
+	sleep 1
+	echo -e "\a"
+	sleep 1
+	echo -e "\a"
+fi
