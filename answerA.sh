6da82a3923daed952468bb28bb4838ec59978d95
luisahurtado <61818251+luisahurtado@users.noreply.github.com>
2020-03-18 14:43:45 +1100

    Add files via upload
    The answer to question 3.a of assignment 1 task 1

---
+++ answerA.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script answer subsection a of question 3
+# Assigment 01 task 01
+# Download program 24 and saves it inot  the ~/bin directory
+# With the name time_signald.sh
+curl -o ~/time_signald.sh https://www.staff.hs-mittweida.de/~wuenschi/data/media/compbiolbook/chapter-10-shell-programming--time-signal.sh
+
