5770d8e99d1561f3b1450eac35e37ef3116cb190
luisahurtado <61818251+luisahurtado@users.noreply.github.com>
2020-03-18 20:11:53 +1100

    Update answerA.sh

--- answerA.sh
+++ answerA.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# This script answer subsection a of question 3
-# Assigment 01 task 01
-# Download program 24 and saves it inot  the ~/bin directory
+# Assignment 1 question 3 subsection a.
+# Download the Program 24 and saves it into the ~/bin directory
 # With the name time_signald.sh
 curl -o ~/time_signald.sh https://www.staff.hs-mittweida.de/~wuenschi/data/media/compbiolbook/chapter-10-shell-programming--time-signal.sh
 
