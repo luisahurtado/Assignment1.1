84ef5149c66c34f567dc82770d67c84efc407028
luisahurtado <61818251+luisahurtado@users.noreply.github.com>
2020-03-18 20:07:00 +1100

    Update newer.sh

--- newer.sh
+++ newer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#Question 1 of the assignment 1.
-#The script resturns the name of the newest file
-#from the documents given as the input
-#Save as newer.sh
+# Question 1 of the assignment 1.
+# The script resturns the name of the newest file
+# from the documents given in the input
+# Save as newer.sh
 documents=$(ls -t $* |head -n 1)
 echo "The newest file in the list is $documents"
